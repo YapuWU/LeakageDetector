11e0a0dee0fd1f8d4479ed9651ec70987fe25e50
Yapu WU <wu96986@gmail.com>
2025-06-10 21:10:44 +1000

    Fully functional

--- main.cpp
+++ main.cpp
@@ -2,10 +2,107 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <avr/sleep.h>
 #include <stdio.h>
+#define PINLED    PB3
+
+#define LED_ON()  (PORTB |= (1 << PINLED))
+#define LED_OFF() (PORTB &= ~(1 << PINLED))
+
+
+#define PINBUZZER PB4
+#define BUZZER_ON() (PORTB |= (1 << PINBUZZER))
+#define BUZZER_OFF() (PORTB &= ~(1 << PINBUZZER))
+
+#define ACGROUND PB1    //Analog Comparator Ground PIN
+#define ACPOSITIVE PB2  //Analog Comparator Positive PIN
+
+ISR(WDT_vect)
+{
+    //Do nothing in watchdog interrupt
+    //This is used to wake up from sleep mode
+}
+
+void init_buzzer()
+{
+    // Set buzzer pin as output
+    DDRB |= (1 << PINBUZZER);
+    BUZZER_OFF(); // Ensure buzzer is off initially
+}
+
+void int_led()
+{
+    // Set LED pin as output
+    DDRB |= (1 << PINLED);
+    LED_OFF(); // Ensure LED is off initially
+}
+void init_analog_comparator()
+{
+    //Disable Digital Input on ACGROUND and ACPOSITIVE
+    DIDR0 |= (1 << AIN1D) | (1 << AIN0D);
+}
+
+//Check if positive input is higher than ground input
+//Before checking, delay 5 clock cycles to allow the comparator to stabilize
+bool IsLeaking()
+{
+    _delay_us(1); // Delay to allow the comparator to stabilize (5 clock cycles at 1 MHz)
+    return (ACSR & (1 << ACO)) != 0; 
+}
+
+//Use watchdog timer to wake up from sleep mode every 4 seconds
+//using interrup and System Reset mode
+void init_watchdog()
+{
+    cli();
+    
+    WDTCR = (1 << WDCE) | (1 << WDE); // Enable watchdog change
+    WDTCR |= (1 << WDTIE) | (1<<WDTIE) | (1 << WDP3) | (0 << WDP2) | (0 << WDP1) | (0 << WDP0); // Enable watchdog interrupt
+    sei();
+}
+
+inline void DisableBOD()
+{
+    // Disable brown-out detection
+    BODCR = (1 << BODS) | (1 << BODSE); // Enable brown-out detection sleep mode
+    BODCR = (1 << BODS); // Disable brown-out detection
+}
 
 int main()
 {
+    //Minimum power consumption
+    PRR |= 1<<PRADC; // Disable ADC
+    PRR |= 1<<PRTIM0; // Disable Timer0
+    //enable pull-up resistors on unused pins
+    PORTB |= (1 << PB3) |  (1 << PB2); // Enable pull-up resistors on unused pins
+    init_buzzer();
+    int_led();
+    while(true)
+    {
+        
+        init_watchdog();
+        // entering powder down mode to save power
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+        sleep_enable();
+        DisableBOD(); // Disable brown-out detection to save power
+        sleep_cpu();     // Enter power down mode
+        sleep_disable(); // Wake up from power down mode
+
+        if(IsLeaking())
+        {
+            BUZZER_ON(); // Turn on buzzer if leak is detected
+            _delay_ms(1000); // Keep buzzer on for 1 second
+            BUZZER_OFF(); // Turn off buzzer
+        }
+        else
+        {
+            LED_ON(); // Turn on LED if no leak is detected
+            BUZZER_OFF(); // Ensure buzzer is off if no leak is detected
+            _delay_ms(100); 
+            LED_OFF(); // Turn off LED
+        }
+    }
+
    return 1;
 }
 
